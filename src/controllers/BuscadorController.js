e900dd0ad66845619c934bba3bd19461e1061d07
juan rios <juan.rios49@eia.edu.co>
2021-08-18 15:46:37 -0500

    user and trip ok

--- src/controllers/BuscadorController.js
+++ src/controllers/BuscadorController.js
@@ -4,7 +4,15 @@ const User=require('../models/User')
 const Trip = require('../models/Trip')
 
 Controller.createFeed = async(req,res)=>{
-
+    Trip({}, function (err, trips) {
+        if (err) {
+            // Devolvemos el código HTTP 404, de producto no encontrado por su id.
+            res.status(203).json({ status: "error", data: "No se ha encontrado el usuario con id: "+req.params.id});
+        } else {
+            // También podemos devolver así la información:
+            res.status(200).json({ status: "ok", data: trips });
+        }
+    })
 }
 
 Controller.filterFeedByDestiny = async(req,res)=>{
